8cf9a133e79c06b364104b67d7bba5984e4779a4
YooChangWoo <dbckddn1201@naver.com>
2024-04-01 17:23:06 +0900

    day 04
    수정

--- day03/sql012_외부조인.sql
+++ day03/sql012_외부조인.sql
@@ -1,11 +1,15 @@
--- CuStomer를 기준으로 Orders 테이블과 외부조인하기
+-- Customer를 기준으로 Orders 테이블과 외부조인하기
+-- Ctrl + Shift + E가 실행
 SELECT c.custid
      , c.[name]
      , c.[address]
      , c.phone
-     , o.orderid
-     , o.custid
      , o.bookid
+     , o.custid -- OUTER JOIN에서는 이 외래키는 필요X
      , o.saleprice
+     , o.orderid
      , o.orderdate
+  FROM Customer AS c RIGHT OUTER JOIN Orders AS o -- LEFT, RIGHT, FULL 등으로 변경하면서 실행
+  -- FROM Customer AS c LEFT OUTER JOIN Orders AS o
+  -- FROM Customer AS c INNER JOIN Orders AS o
     ON c.custid = o.custid;
